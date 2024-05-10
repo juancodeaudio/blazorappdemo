dd9386b31597e20e60ef1e744c0e658952b4e631
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-05-09 21:34:50 -0500

    Add apiUrl configuration to use in HttpClient

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using blazorappdemo;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 
 await builder.Build().RunAsync();

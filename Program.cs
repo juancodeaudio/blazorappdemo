f1388e89a1ce16fb1bc3d914e23246f37b79ad5b
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2024-05-21 22:18:27 -0500

    Add BlazoredToast component and service, and show success toast when deleting a product

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiUrl = builder.Configuration.GetValue<string>("apiUrl");
 
+builder.Services.AddBlazoredToast();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
